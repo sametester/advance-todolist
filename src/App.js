eaba9b4863b0909f5ae0182773b6ad23e3631ca6
sametester <sittichok.boo@gmail.com>
2022-08-23 09:48:43 +0700

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoContainer from './components/TodoContainer';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5 mb-3" style={{ maxWidth: 576 }}>
+      <div className="my-4">
+        <TodoForm />
+      </div>
+      <TodoContainer />
+      <TodoList />
+
+      <div className="my-2 d-flex justify-content-between align-items-center">
+        <small className="text-muted">Showing 6 to 10 of 12 entries</small>
+        {/* ********** PAGINATION ********** */}
+        <ul className="pagination pagination-sm mb-0">
+          <li className="page-item">
+            <button className="page-link">
+              <i className="fa-solid fa-angle-left small" />
+            </button>
+          </li>
+          <li className="page-item">
+            <button className="page-link">1</button>
+          </li>
+          <li className="page-item active">
+            <button className="page-link">2</button>
+          </li>
+          <li className="page-item">
+            <button className="page-link">3</button>
+          </li>
+          <li className="page-item">
+            <button className="page-link">
+              <i className="fa-solid fa-angle-right small" />
+            </button>
+          </li>
+        </ul>
+        {/* ********** END PAGINATION ********** */}
+      </div>
     </div>
   );
 }
