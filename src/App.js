052a1f2bd77f198e1d6bbae8892d7e7b6d09cbc1
sametester <sittichok.boo@gmail.com>
2022-08-23 10:07:38 +0700

    edit component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Pagination from './components/Pagination';
 import TodoContainer from './components/TodoContainer';
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
@@ -14,27 +15,7 @@ function App() {
       <div className="my-2 d-flex justify-content-between align-items-center">
         <small className="text-muted">Showing 6 to 10 of 12 entries</small>
         {/* ********** PAGINATION ********** */}
-        <ul className="pagination pagination-sm mb-0">
-          <li className="page-item">
-            <button className="page-link">
-              <i className="fa-solid fa-angle-left small" />
-            </button>
-          </li>
-          <li className="page-item">
-            <button className="page-link">1</button>
-          </li>
-          <li className="page-item active">
-            <button className="page-link">2</button>
-          </li>
-          <li className="page-item">
-            <button className="page-link">3</button>
-          </li>
-          <li className="page-item">
-            <button className="page-link">
-              <i className="fa-solid fa-angle-right small" />
-            </button>
-          </li>
-        </ul>
+        <Pagination />
         {/* ********** END PAGINATION ********** */}
       </div>
     </div>
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,21 +1,17 @@
 function TodoForm() {
   return (
-    <div className="my-4">
-      {/* ********** ADD TODO FORM ********** */}
-      <form>
-        <div className="input-group">
-          <input type="text" className="form-control" />
-          <button className="btn btn-primary">
-            <i className="fa-solid fa-check" />
-          </button>
-          <button className="btn btn-outline-secondary">
-            <i className="fa-solid fa-xmark" />
-          </button>
-        </div>
-        {/* <small className="text-danger">Title is required.</small> */}
-      </form>
-      {/* ********** END ADD TODO FORM ********** */}
-    </div>
+    <form>
+      <div className="input-group">
+        <input type="text" className="form-control" />
+        <button className="btn btn-primary">
+          <i className="fa-solid fa-check" />
+        </button>
+        <button className="btn btn-outline-secondary">
+          <i className="fa-solid fa-xmark" />
+        </button>
+      </div>
+      {/* <small className="text-danger">Title is required.</small> */}
+    </form>
   );
 }
 
