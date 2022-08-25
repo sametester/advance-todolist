a900518263e7e2a47ed3a9a7d902e79185adde7f
sametester <sittichok.boo@gmail.com>
2022-08-25 10:53:09 +0700

    create component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,29 @@
+import { Fragment } from 'react';
+import PageLimit from './PageLimit';
+import SearchStatus from './SearchStatus';
+import SearchText from './SearchText';
+import Sort from './Sort';
+import TodoList from './TodoList';
+import Pagination from './Pagination';
+
+function TodoContainer() {
+  return (
+    <Fragment>
+      <div className="my-2 d-flex gap-3">
+        <SearchText />
+        <SearchStatus />
+      </div>
+      <div className="my-2 d-flex justify-content-between">
+        <PageLimit />
+        <Sort />
+      </div>
+      <TodoList />
+      <div className="my-2 d-flex justify-content-between align-items-center">
+        <small className="text-muted">Showing 6 to 10 of 12 entries</small>
+        <Pagination />
+      </div>
+    </Fragment>
+  );
+}
+
+export default TodoContainer;
