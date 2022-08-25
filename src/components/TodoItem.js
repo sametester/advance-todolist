a4af22ba3c52e22e678846ed56bf27d9a94aec27
sametester <sittichok.boo@gmail.com>
2022-08-25 10:13:26 +0700

    add

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,19 +2,19 @@ import { useState } from 'react';
 import TodoContent from './TodoContent';
 import TodoForm from './TodoForm';
 
-function TodoItem({ todo }) {
+function TodoItem({ todo, editingTodo, openEdit }) {
   const { completed, id } = todo;
-  const [isEditing, setIsEditing] = useState({});
+
   return (
     <li
       className={`list-group-item p-3 callout-${
         completed ? 'success' : 'warning'
       }`}
     >
-      {isEditing.id === id ? (
+      {editingTodo.id === id ? (
         <TodoForm />
       ) : (
-        <TodoContent todo={todo} openEdit={() => setIsEditing(todo)} />
+        <TodoContent todo={todo} openEdit={openEdit} />
       )}
     </li>
   );
